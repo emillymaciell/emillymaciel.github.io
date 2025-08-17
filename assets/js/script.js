3e24322f2b97b04626ab0b0a24353ce0f29db93f
Emilly Maciel <emilly.m.limma@gmail.com>
2025-08-17 18:33:57 -0300

    finished portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,47 @@
 const toggleTheme = document.getElementById('toggleTheme');
 const rootHtml = document.documentElement;
+const accordionHeaders = document.querySelectorAll('.accordion__header');
+const menuLinks = document.querySelectorAll(".menu__link");
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme) {
+    rootHtml.setAttribute("data-theme", savedTheme);
+    if (savedTheme === "dark") {
+        toggleTheme.classList.add("bi-sun");
+        toggleTheme.classList.remove("bi-moon");
+    } else {
+        toggleTheme.classList.add("bi-moon");
+        toggleTheme.classList.remove("bi-sun");
+    }
+}
 
 function changeTheme() {
     const currentTheme = rootHtml.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
 
-    currentTheme == 'dark' ? rootHtml.setAttribute('data-theme', 'light') : rootHtml.setAttribute('data-theme', 'dark')
+    rootHtml.setAttribute('data-theme', newTheme);
 
     toggleTheme.classList.toggle('bi-sun')
     toggleTheme.classList.toggle('bi-moon')
+
+
+    localStorage.setItem("theme", newTheme);
 }
 
 toggleTheme.addEventListener('click', changeTheme);
+
+accordionHeaders.forEach(header => {
+    header.addEventListener('click', () => {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains('active');
+
+        accordionActive ? accordionItem.classList.remove('active') : accordionItem.classList.add('active');
+    })
+})
+
+menuLinks.forEach(item => {
+    item.addEventListener("click", () => {
+        menuLinks.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    })
+})
